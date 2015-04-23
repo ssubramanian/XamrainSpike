65e150abd0abdebbdc56c982ac471d6cd15f902d
Senthil Kumar Subramanian <ssubramanian@daxko.com>
2015-04-23 14:06:55 -0500

    first commit

---
+++ XamrainSpike/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace XamrainSpike
+{
+	[Activity(Label = "XamarinSpike", MainLauncher = true, Icon = "@drawable/icon", Theme="@style/CustomActionBarTheme")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			//Set our custom view
+			ActionBar.SetCustomView(Resource.Layout.action_bar);
+			ActionBar.SetDisplayShowCustomEnabled(true);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+
+		}
+	}
+}
+
+
